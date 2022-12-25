7257a6e813e5ba89f9ecb67d21660233b1517b83
antyfg5 <118511713+antyfg5@users.noreply.github.com>
2022-12-25 19:22:38 +0700

    Add files via upload

---
+++ UIManager2.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager2 : MonoBehaviour
+{
+	// Start is called before the first frame update
+	public void Settings()
+	{
+		SceneManager.LoadScene(2);
+	}
+
+	// Update is called once per frame
+
+
+
+
+}
